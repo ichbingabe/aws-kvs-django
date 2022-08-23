aa4da816415ffc96166a46c2777a77efb5ed3593
Gabriel Esteves <brielesteves@gmail.com>
2022-08-23 17:28:04 -0300

    settings changes

--- django_aws_video_stream/settings.py
+++ django_aws_video_stream/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'npa^x&&cfrs6uavq7g2*f+p&&@@a-$+i@cc6=f%*swq_8e9fit'
+SECRET_KEY = 'put your secret key here '
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
