daa1e9848b292ee971ffa6e8d4a0538a268cda8e
Gabriel Esteves <brielesteves@gmail.com>
2022-08-23 12:17:15 -0300

    initial commit

---
+++ aws_services/services.py
@@ -0,0 +1,64 @@
+import boto3
+import botocore.exceptions
+from botocore.exceptions import ClientError
+
+class KinesisVideoStream:
+    def __init__(self, channel_name):
+        self.channel_name = channel_name
+
+    def describe_chanenel(self):
+        client = boto3.client('kinesisvideo')
+        try :
+            response = client.describe_signaling_chanenel(
+                ChannelName=self.channel_name
+            )
+            return response
+        except botocore.exceptions.ClientError as err:
+            if err.response['Error']['Code'] == 'ResourceNotFoundException':
+                return False
+            else:
+                raise(err)
+        except Exception as er:
+            raise(er)
+
+    def create_channel(self):
+        client = boto3.client('kinesisvideo')
+        try:
+            client.create_signaling_channel(
+            ChannelName=self.channel_name,
+            ChannelType='SINGLE_MASTER',
+            SingleMasterConfiguration={
+                'MessageTtlSeconds': 15
+            }
+            )
+            return True
+        except Exception as er:
+            raise(er)
+
+    def delete_channel(self):
+        client = boto3.client('kinesisvideo')
+        response = self.describe_channel(self.channel_name)
+        if response == False:
+            return False # this channel not exists
+        else:
+            channel_info = response['ChannelInfo']
+            channel_version = channel_info['Version']
+            channel_arn = channel_info['ChannelARN']
+            
+            try:
+                client.delete_signaling_channel(
+                ChannelARN=channel_arn,
+                CurrentVersion=channel_version
+                )
+                return True # this channel is deleted
+            
+            except botocore.exceptions.ClientError as error:
+                # this channel is current in use
+                if error.response['Error']['Code'] == 'ResourceInUseException':
+                    pass
+                #    raise(ChannelInUseException)
+                else:
+                    raise(error)
+                
+            except Exception as er:
+                raise(er)
