5de52febdb188dcb9bda3b49773618eba0199e2c
Gabriel Esteves <brielesteves@gmail.com>
2022-08-24 17:01:28 -0300

    add consumer

--- consumer/urls.py
+++ consumer/urls.py
@@ -5,5 +5,4 @@ from consumer import views
 
 urlpatterns = [
     url(r'create-channel/', views.consumerView.open_signaling_channel, name='create_channel'),
-    url(r'delete-channel/', views.classRoomView.delete_signaling_channel, name='delete_channel')
 ]
