38b6e093c87695c227ff177cab6b93b09971107e
Gabriel Esteves <brielesteves@gmail.com>
2022-08-25 10:31:54 -0300

    add delete_channel

--- consumer/views.py
+++ consumer/views.py
@@ -8,11 +8,22 @@ from aws_services.services import KinesisVideoStream as kvs
 # Create your views here.
 class consumerView(APIView):
     @api_view(('POST', ))
-    def open_signaling_channel(req):
+    def create_signaling_channel(req):
         params = req.data 
         channel_name = params['channel_name']
-        response = kvs(channel_name).create_channel()
-        if(response):
-            response = {"response":"the channel is successfully created"}
-            return Response(data=response, status=status.HTTP_201_CREATED)
+        res = kvs(channel_name).create_channel()
+        if(res):
+            res = {"response":"the channel is successfully created"}
+            return Response(data=res, status=status.HTTP_201_CREATED)
 
+    @api_view(('POST', ))
+    def delete_signaling_channel(req):
+        params = req.data 
+        channel_name = params['channel_name']
+        res = kvs(channel_name).delete_channel()
+        if(res):
+            res = {"response":"the channel is successfully deleted"}
+            return Response(data=res, status=status.HTTP_200_OK)
+        else:
+            res = {"response":"this channel does not exists!"}
+            return Response(data=res)
